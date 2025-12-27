86fc81d99397954efd65911f8bae1d2ecec9e61b
Elias <3elias3rd@gmail.com>
2025-12-27 11:18:13 +0400

    Added functions to read, add and delete todos

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
-from fastapi.exceptions import HTTPException
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from typing import Optional
+from pathlib import Path
+import json
 
-app = FastAPI(title="To Do App")
+app = FastAPI(title="Clean To Do App")
 
-@app.get("/")
-def hello():
-    return {"message": "hello world"}
+templates = Jinja2Templates(directory="templates")
+
+TODO_FILE = Path("todos.json")
+
+def load_todos():
+    """Load todos from JSON file"""
+    if TODO_FILE.exists():
+        with open(TODO_FILE, 'r') as f:
+            return json.load(f)
+    return []
+
+def save_todos(todos):
+    """Sava todos to JSON file"""
+    with open(TODO_FILE, 'w') as f:
+        json.dump(todos, f, indent=2)    
